f22c195a2065b3937699e8cf4874b445e00f7c97
Karan Jaiswal <karanjaiswal632@gmail.com>
2016-03-11 04:59:02 +0100

    statement types

---
+++ Statement.java
@@ -0,0 +1,16 @@
+public class Statement{
+
+	public static void main(String args[]){
+
+		String pwd="man";
+
+		if(pwd=="karan"){
+			System.out.println("You are Welcome to your World");
+
+			}
+			else{
+
+				System.out.println("Invalid password->You are thief");
+			}
+		}
+}
